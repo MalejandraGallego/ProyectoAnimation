54fe3ce9f49ad219380146c8735015bb53b663c7
MalejandraGallego <maleja.mg7@gmail.com>
2026-05-15 01:47:51 -0500

    Sagrega carpeta juego reto

---
+++ juego/Program.fs
@@ -0,0 +1,194 @@
+﻿open System
+open System.Threading
+
+open App.Utils
+
+type ProgramState =
+| Running
+| Terminated
+
+type Misil = {
+    X: int
+    Y: int
+}
+type State = {
+    ProgramState: ProgramState
+    AlienX: int
+    AlienY: int
+    RedibujarPantalla: bool
+    Tick: int
+    Misiles: Misil list
+    EnemigoX: int
+    EnemigoY: int
+    EnemigoDir: int
+    MisilesEnemigo: Misil list  // se agrega para misiles enemigos
+}
+
+let estadoInicial = {
+    ProgramState = Running
+    AlienX = Console.BufferWidth/2
+    AlienY = Console.BufferHeight/2
+    RedibujarPantalla = true
+    Tick = -1
+    Misiles = []
+    EnemigoX= Console.BufferWidth-2
+    EnemigoY= 0
+    EnemigoDir=1
+    MisilesEnemigo = [] // se agrega para misiles enemigo
+}
+
+let actualizarTick state =
+    {state with Tick = state.Tick+1}
+
+
+let actualizarMisiles state =
+    if state.Misiles <> [] then 
+        state.Misiles
+        |> Seq.map (fun misil -> {misil with X = misil.X+1})
+        |> Seq.filter (fun misil -> misil.X < Console.BufferWidth-2)
+        |> Seq.toList
+        |> fun nuevosMisiles ->
+            {state with Misiles = nuevosMisiles; RedibujarPantalla=true}
+    else
+        state
+
+
+//Se agrega para misiles del enemigo
+
+let actualizarMisilesEnemigo state =  
+    
+    let misilesMovidos =
+        state.MisilesEnemigo
+        |> List.map (fun m -> {m with X = m.X - 1})
+        |> List.filter (fun m -> m.X > 0)
+
+    let misilesConDisparo =
+        if state.Tick % 20 = 0 then
+            let nuevoMisil = { X = state.EnemigoX - 1; Y = state.EnemigoY }
+            nuevoMisil :: misilesMovidos
+        else
+            misilesMovidos
+
+    if misilesConDisparo <> state.MisilesEnemigo then
+        {state with MisilesEnemigo = misilesConDisparo; RedibujarPantalla = true}
+    else
+        state
+
+
+let actualizarEnemigo state =
+    if state.Tick % 4 = 0 then 
+        let nuevoY = state.EnemigoY + state.EnemigoDir
+        let nuevaDir,Y = 
+            match nuevoY with 
+            | y when y > Console.BufferHeight-1 -> -1,Console.BufferHeight-1
+            | y when y < 0 -> 1,0
+            | _ -> state.EnemigoDir,nuevoY
+
+
+        {state with EnemigoY = Y; EnemigoDir=nuevaDir;RedibujarPantalla=true}
+    else
+        state
+
+
+
+let procesearTecladoApp key state =
+    match key with 
+    | ConsoleKey.Escape ->
+        {state with ProgramState = Terminated}
+    | _ -> state
+let procesarTecladoDeAlien key state =
+    match key with 
+    | ConsoleKey.UpArrow ->
+        {state with AlienY = max 0 (state.AlienY-1)}
+    | ConsoleKey.DownArrow ->
+        {state with AlienY = min (Console.BufferHeight-1) (state.AlienY+1)}
+
+    | ConsoleKey.LeftArrow ->
+        {state with AlienX = max 0 (state.AlienX-1)}
+    | ConsoleKey.RightArrow ->
+        {state with AlienX = min (Console.BufferWidth-2) (state.AlienX+1)}
+
+    | ConsoleKey.Spacebar ->
+        let nuevoMisil = {
+            X = state.AlienX+2
+            Y = state.AlienY
+        }
+        {state with Misiles = nuevoMisil :: state.Misiles}
+        
+    | _ ->
+        state
+    |> fun newState ->
+        if newState <> state then 
+            {newState with RedibujarPantalla=true}
+        else
+            state
+
+let procesarTeclado state =
+    if Console.KeyAvailable then 
+        let k = Console.ReadKey true
+        state 
+        |> procesearTecladoApp k.Key
+        |> procesarTecladoDeAlien k.Key
+    else
+        state
+
+let redibujarAlien state =
+    mostrarMensaje state.AlienX state.AlienY ConsoleColor.Yellow "👽"
+
+let redibujarMisiles state =
+    
+    state.Misiles
+    |> List.iter (fun misil ->
+        mostrarMensaje misil.X misil.Y ConsoleColor.Yellow "=>"
+    )
+
+
+// se agrega para dibujar los misiles del enemigo
+
+let redibujarMisilesEnemigo state =
+    state.MisilesEnemigo
+    |> List.iter (fun misil ->
+        mostrarMensaje misil.X misil.Y ConsoleColor.Red "<="
+    )
+
+
+let redibujarEnemigo state =
+    mostrarMensaje state.EnemigoX state.EnemigoY ConsoleColor.Yellow "☠️"
+
+let redibujarPantalla state =
+    if state.RedibujarPantalla then 
+        Console.Clear()
+        [|
+            redibujarMisiles
+            redibujarMisilesEnemigo  // agregamos para redibujar misiles enemigo
+            redibujarAlien
+            redibujarEnemigo
+        |] |> Array.iter ( fun f -> state |> f)
+        {state with RedibujarPantalla=false}
+    else
+        state
+
+
+let rec mainLoop state =
+    let newState =
+        state 
+        |> actualizarTick
+        |> actualizarMisiles
+        |> actualizarMisilesEnemigo // se agrega actualziar misilesenemigo
+        |> actualizarEnemigo
+        |> procesarTeclado
+        |> redibujarPantalla
+    if newState.ProgramState <> Terminated then 
+        Thread.Sleep 25
+        newState |> mainLoop
+
+Console.Clear()
+Console.CursorVisible <- false
+
+estadoInicial
+|> mainLoop
+
+Console.CursorVisible <- true
+Console.Clear()
+
+
